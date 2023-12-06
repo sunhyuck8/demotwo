bb7d3396c2f699593c8f3102bd828db51369ca26
Sun-Hyuck-Yoo <awfullysh82@gmail.com>
2023-12-06 20:36:58 +0900

    first commit

---
+++ src/main/java/com/example/demoTow/DemoTowApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoTow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoTow/DemoTowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoTow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
